854fc479807a676e94c8639ec130290c3a583fd3
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2023-02-01 08:19:21 +0530

    Search Component Updated

--- Components/UiComponents/Home/Search.jsx
+++ Components/UiComponents/Home/Search.jsx
@@ -1,9 +1,13 @@
-import { View, Text, Dimensions, StyleSheet } from 'react-native'
-import React from 'react'
+import { React, useState } from 'react'
+import { View, Text, Dimensions, StyleSheet, Image, TextInput } from 'react-native'
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
 
 const { width, height } = Dimensions.get('window')
 
 export default function Search() {
+
+    const [selectedCity, setSelectedCity] = useState('Gwalior')
+
     return (
         <View style={{
             backgroundColor: "linear-gradient(270deg, rgba(72,167,158,1) 4%, rgba(0,255,235,1) 100%)",
@@ -18,9 +22,34 @@ export default function Search() {
                             10 Lac+ Happy Customer
                         </Text>
                     </View>
-                    <View></View>
-                    <View></View>
-                    <View></View>
+                    <View style={{ marginTop: 4, }}>
+                        <View style={{ justifyContent: 'center', alignItems: 'center', }}>
+                            <Image
+                                style={{ width: 164, height: 72, resizeMode: 'contain' }}
+                                source={require('../../Assets/Rentals1.png')}
+                            />
+                            <Text style={{ fontSize: 20, fontWeight: 500, color: "#fff", letterSpacing: 1.2, marginTop: 4, }}>
+                                Let's Find a Car for You
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={{
+                        width: width * 0.92, backgroundColor: '#fff', borderRadius: 32, borderWidth: 0.5, borderColor: '#3498db', padding: 8, marginTop: 18, display: 'flex', flexDirection: 'row', alignItems: 'center',
+                    }}>
+                        <Icon name="location-enter" style={{ fontSize: 36, color: "#000", }} />
+                        <TextInput
+                            style={{
+                                backgroundColor: "#fff", fontSize: 30, marginLeft: 12, color: "#000", marginTop: -1,
+                            }}
+                            value={selectedCity} multiline={true}
+                        />
+                        <Icon name="arrow-right" style={{ fontSize: 36, color: "#000", marginLeft: 142, }} />
+                    </View>
+                    <View style={{ marginTop: 18, }}>
+                        <Text style={{ fontSize: 20, fontWeight: 500, color: "#fff", letterSpacing: 1.2, marginTop: 4, }}>
+                            Want to book in different City?
+                        </Text>
+                    </View>
                 </View>
             </View>
         </View>
