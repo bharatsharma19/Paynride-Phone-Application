3f936ee0f856dce6a1317768fa7375981f026464
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2023-02-01 14:47:39 +0530

    Updated

---
+++ Components/Navigation/RootNavigator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Login from '../Screens/Login';
+import Home from '../Screens/Home';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+export default function RootNavigator() {
+  const ProjectDrawer = () => {
+    return (
+      <Drawer.Navigator
+        drawerContent={props => <CustomDrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={Home} />
+      </Drawer.Navigator>
+    );
+  };
+
+  const CustomDrawerContent = props => {
+    return (
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props}>
+          <DrawerItem
+            label="My Profile"
+            icon={<Icon name="account-details-outline" size={20} />}
+          />
+          <DrawerItem
+            label="Settings"
+            icon={<Icon name="account-settings" size={20} />}
+          />
+          <DrawerItem label="Logout" icon={<Icon name="Logout" size={20} />} />
+        </DrawerItemList>
+      </DrawerContentScrollView>
+    );
+  };
+
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={ProjectDrawer} />
+    </Stack.Navigator>
+  );
+}
