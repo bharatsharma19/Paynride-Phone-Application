24ce50b90b50eeec562e034826200ef33f202bde
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2023-02-03 20:04:48 +0530

    App Header Created

--- Components/Navigation/RootNavigator.js
+++ Components/Navigation/RootNavigator.js
@@ -9,6 +9,8 @@ import {
   DrawerItemList,
   DrawerItem,
 } from '@react-navigation/drawer';
+import {Text, Image, View} from 'react-native';
+import AppHeader from '../UiComponents/Common/AppHeader';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -18,7 +20,11 @@ export default function RootNavigator() {
     return (
       <Drawer.Navigator
         drawerContent={props => <CustomDrawerContent {...props} />}>
-        <Drawer.Screen name="HomePage" component={Home} />
+        <Drawer.Screen
+          name="HomePage"
+          component={Home}
+          options={{headerShown: false}}
+        />
       </Drawer.Navigator>
     );
   };
@@ -26,6 +32,26 @@ export default function RootNavigator() {
   const CustomDrawerContent = props => {
     return (
       <DrawerContentScrollView {...props}>
+        <View
+          style={{
+            display: 'flex',
+            padding: 20,
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}>
+          <Image
+            style={{
+              marginBottom: 5,
+              borderRadius: 50,
+              resizeMode: 'contain',
+              width: 100,
+              height: 100,
+            }}
+            source={require('../Assets/transparent_logo.png')}
+          />
+          <Text>+910000000000</Text>
+          <Text style={{fontSize: 12}}>paynride1909@gmail.com</Text>
+        </View>
         <DrawerItemList {...props} />
         <DrawerItem
           label="My Profile"
@@ -44,11 +70,15 @@ export default function RootNavigator() {
   };
 
   return (
-    <Stack.Navigator
-      initialRouteName="Home"
-      screenOptions={{headerShown: false}}>
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Home" component={ProjectDrawer} />
+      <Stack.Screen
+        name="Home"
+        component={ProjectDrawer}
+        options={{
+          header: AppHeader,
+        }}
+      />
     </Stack.Navigator>
   );
 }
---
+++ Components/UiComponents/Common/AppHeader.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    Image,
+    Dimensions,
+    View,
+} from 'react-native';
+import MCI from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useNavigation } from '@react-navigation/native'
+import { DrawerActions } from '@react-navigation/native'
+const { width, height } = Dimensions.get('window')
+
+export default function AppHeader() {
+
+    var navigation = useNavigation()
+
+    return (
+        <View>
+            <View style={{ alignItems: 'center', backgroundColor: '#fff', display: 'flex', width: width, height: height * 0.08, justifyContent: 'space-between', flexDirection: 'row', padding: 5 }}>
+                <MCI name="menu" size={24} onPress={() => navigation.dispatch(DrawerActions.openDrawer())} />
+                <Image style={{ resizeMode: 'contain', width: 72, height: 120 }}
+                    source={require('../../Assets/classic.png')} />
+                <MCI name="account" size={24} />
+            </View>
+        </View>
+    )
+}
