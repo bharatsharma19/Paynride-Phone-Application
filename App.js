5cffe298ee26b0c94d0f11b6d3baaa9f7db0d418
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2023-02-03 19:36:39 +0530

    Updated

--- App.js
+++ App.js
@@ -2,23 +2,15 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {React} from 'react';
 import {View, Dimensions} from 'react-native';
-import Home from './Components/Screens/Home';
-import Login from './Components/Screens/Login';
+import RootNavigator from './Components/Navigation/RootNavigator';
 
 const {width, height} = Dimensions.get('window');
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View
-      style={{backgroundColor: '#fff', width: width * 1, height: height * 1}}>
+    <View style={{backgroundColor: '#fff', width: width * 1, height: height}}>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
+        <RootNavigator />
       </NavigationContainer>
     </View>
   );
