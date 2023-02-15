37c131122ed5dd955e033b04bf76d2e8a15a8457
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2023-02-15 09:45:42 +0530

    Stylesheet Updated of Login Screen

--- Components/Screens/Login.jsx
+++ Components/Screens/Login.jsx
@@ -117,20 +117,7 @@ const Login = ({ navigation }) => {
 
                                 <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
 
-                                    <View style={{
-                                        width: width * 0.48,
-                                        backgroundColor: '#fff',
-                                        borderRadius: 32,
-                                        borderWidth: 1,
-                                        borderColor: '#3498db',
-                                        padding: 10,
-                                        marginTop: 12,
-                                        display: 'flex',
-                                        flexDirection: 'row',
-                                        alignItems: 'center',
-                                        marginBottom: 6,
-                                        marginLeft: 12,
-                                    }}>
+                                    <View style={styles.otpContainer}>
                                         <Icon name={'cellphone-key'} style={{ fontSize: 22, color: "#000", marginRight: 4, }} />
                                         <TextInput
                                             style={{ fontSize: 16, color: "#000" }}
@@ -154,42 +141,22 @@ const Login = ({ navigation }) => {
 
     return (
         <>
-            <View style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignContent: 'center',
-                marginTop: 24,
-                height: height * 0.32,
-                width: width * 0.84,
-                borderRadius: 16,
-                marginTop: '32%',
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                backgroundColor: '#f2f2f2',
-                opacity: modalVisible ? 0.1 : 1,
-            }}>
-                <View style={{ marginLeft: width * 0.21 }}>
-                    <Image
-                        style={{ width: 164, height: 72, resizeMode: 'contain' }}
-                        source={require('../Assets/Rentals1.png')}
-                    />
-                </View>
-                <Text style={{
-                    marginTop: 16,
-                    paddingVertical: 8,
-                    color: '#20232a',
-                    textAlign: 'center',
-                    fontSize: 22,
-                    fontWeight: 'bold',
-                    fontFamily: 'Poppins',
-                    marginBottom: 4,
-                }}>
-                    Login
-                </Text>
-                <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
-                    <Input iconName="phone" placeholder="Mobile Number" keyboardType="numeric" error={error.mobileEmail} onFocus={() => handleErrors(null, "mobileEmail")} onChangeText={(txt) => handleValues(txt, 'mobileEmail')} />
+            <View style={styles.mainContainer}>
+                <View style={{ opacity: modalVisible ? 0.1 : 1, }}>
+                    <View style={{ marginLeft: width * 0.21 }}>
+                        <Image
+                            style={{ width: 164, height: 72, resizeMode: 'contain' }}
+                            source={require('../Assets/Rentals1.png')}
+                        />
+                    </View>
+                    <Text style={styles.loginTxt}>
+                        Login
+                    </Text>
+                    <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+                        <Input iconName="phone" placeholder="Mobile Number" keyboardType="numeric" error={error.mobileEmail} onFocus={() => handleErrors(null, "mobileEmail")} onChangeText={(txt) => handleValues(txt, 'mobileEmail')} />
 
-                    <AppButton onPress={handleOtpClick} btnWidth={0.78} buttonText={'Get Otp'} bgColor='#2980b9' borderRadius={24} />
+                        <AppButton onPress={handleOtpClick} btnWidth={0.78} buttonText={'Get Otp'} bgColor='#2980b9' borderRadius={24} />
+                    </View>
                 </View>
             </View>
             {otpModal()}
@@ -198,6 +165,31 @@ const Login = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
+    mainContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        marginTop: 24,
+        height: height * 0.32,
+        width: width * 0.84,
+        borderRadius: 16,
+        marginTop: '32%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        backgroundColor: '#f2f2f2',
+    },
+
+    loginTxt: {
+        marginTop: 16,
+        paddingVertical: 8,
+        color: '#20232a',
+        textAlign: 'center',
+        fontSize: 22,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins',
+        marginBottom: 4,
+    },
+
     centeredView: {
         flex: 1,
         display: "flex",
@@ -221,6 +213,21 @@ const styles = StyleSheet.create({
         elevation: 5,
         height: height * 0.36,
         width: width * 0.72,
+    },
+
+    otpContainer: {
+        width: width * 0.48,
+        backgroundColor: '#fff',
+        borderRadius: 32,
+        borderWidth: 1,
+        borderColor: '#3498db',
+        padding: 10,
+        marginTop: 12,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 6,
+        marginLeft: 12,
     },
 });
 
