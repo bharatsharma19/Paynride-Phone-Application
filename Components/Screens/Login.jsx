17ccc35cceb401ec724f344baa8f23d3149132d8
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2023-02-16 21:42:10 +0530

    Login Screen Updated

--- Components/Screens/Login.jsx
+++ Components/Screens/Login.jsx
@@ -31,8 +31,20 @@ const Login = ({ navigation }) => {
     }
 
     const [userDetails, setUserDetails] = useState([])
+
+    var [valueOne, setValueOne] = useState("")
+    var [valueTwo, setValueTwo] = useState("")
+    var [valueThree, setValueThree] = useState("")
+    var [valueFour, setValueFour] = useState("")
+    var [valueFive, setValueFive] = useState("")
+    var [valueSix, setValueSix] = useState("")
+
     const [generatedOtp, setGeneratedOtp] = useState("")
     const [enteredOtp, setEnteredOtp] = useState("")
+
+    const handleEnteredOtp = (txt) => {
+        setEnteredOtp(parseInt(valueOne + valueTwo + valueThree + valueFour + valueFive + txt))
+    }
 
     const checkUser = async (mobileNumber) => {
         var response = await postData("user/checkuser", { mobileno: mobileNumber });
@@ -61,8 +73,10 @@ const Login = ({ navigation }) => {
     const handleLoginClick = async () => {
         let tempOtp = await parseInt(enteredOtp)
 
+        console.log(enteredOtp)
+        console.log(tempOtp)
+
         if (generatedOtp === tempOtp) {
-            //console.log(userDetails)
             setModalVisible(false)
 
             if (userDetails.status) {
@@ -99,42 +113,88 @@ const Login = ({ navigation }) => {
     function otpModal() {
         return (
             <>
-                <View style={styles.centeredView}>
-                    <Modal
-                        animationType="fade"
-                        transparent={true}
-                        visible={modalVisible}
-                        onRequestClose={() => {
-                            Alert.alert('City Selected!');
-                            setModalVisible(!modalVisible);
-                        }} >
-                        <View style={styles.centeredView}>
-                            <View style={styles.modalView}>
+                <Modal
+                    animationType="fade"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('City Selected!');
+                        setModalVisible(!modalVisible);
+                    }} >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
 
-                                <Text style={{ fontSize: 24, fontWeight: 500, color: "#000", }}>
-                                    Enter Otp to Continue...
-                                </Text>
+                            <Text style={{ fontSize: 24, fontWeight: 700, color: "#000", }}>
+                                Verification
+                            </Text>
+                            <Text style={{ fontSize: 16, fontWeight: 500, color: "#353b48", }}>
+                                A 6-Digit Otp has been sent to your mobile number. Enter it below to Continue...
+                            </Text>
+                            <View style={styles.innerContainer}>
 
-                                <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+                                <View style={styles.otpMainContainer}>
 
                                     <View style={styles.otpContainer}>
-                                        <Icon name={'cellphone-key'} style={{ fontSize: 22, color: "#000", marginRight: 4, }} />
                                         <TextInput
-                                            style={{ fontSize: 16, color: "#000" }}
-                                            placeholder={'Enter Otp'}
+                                            style={{ fontSize: 24, color: "#000" }}
+                                            cursorColor='#000'
                                             keyboardType={'number-pad'}
-                                            placeholderTextColor="#000"
-                                            maxLength={6} onChangeText={(txt) => setEnteredOtp(txt)}
-                                            value={enteredOtp}
+                                            maxLength={1} onChangeText={(txt) => setValueOne(txt)}
+                                            value={valueOne}
+                                            color='#000'
+                                        />
+                                    </View>
+                                    <View style={styles.otpContainer}>
+                                        <TextInput
+                                            style={{ fontSize: 24, color: "#000" }}
+                                            keyboardType={'number-pad'}
+                                            maxLength={1} onChangeText={(txt) => setValueTwo(txt)}
+                                            value={valueTwo}
+                                        />
+                                    </View>
+                                    <View style={styles.otpContainer}>
+                                        <TextInput
+                                            style={{ fontSize: 24, color: "#000" }}
+                                            keyboardType={'number-pad'}
+                                            maxLength={1} onChangeText={(txt) => setValueThree(txt)}
+                                            value={valueThree}
+                                        />
+                                    </View>
+                                    <View style={styles.otpContainer}>
+                                        <TextInput
+                                            style={{ fontSize: 24, color: "#000" }}
+                                            keyboardType={'number-pad'}
+                                            maxLength={1} onChangeText={(txt) => setValueFour(txt)}
+                                            value={valueFour}
+                                        />
+                                    </View>
+                                    <View style={styles.otpContainer}>
+                                        <TextInput
+                                            style={{ fontSize: 24, color: "#000" }}
+                                            keyboardType={'number-pad'}
+                                            maxLength={1} onChangeText={(txt) => setValueFive(txt)}
+                                            value={valueFive}
+                                        />
+                                    </View>
+                                    <View style={styles.otpContainer}>
+                                        <TextInput
+                                            style={{ fontSize: 24, color: "#000" }}
+                                            keyboardType={'number-pad'}
+                                            maxLength={1}
+                                            onChangeText={(txt) => handleEnteredOtp(txt)}
+                                            value={valueSix}
                                         />
                                     </View>
 
-                                    <AppButton onPress={handleLoginClick} btnWidth={0.54} buttonText={'Sign In'} bgColor='#2980b9' borderRadius={24} />
+                                </View>
+
+                                <View style={{ marginTop: 24, }}>
+                                    <AppButton onPress={handleLoginClick} btnWidth={0.72} buttonText={'Continue'} bgColor='#2980b9' borderRadius={24} />
                                 </View>
                             </View>
                         </View>
-                    </Modal>
-                </View>
+                    </View>
+                </Modal>
             </>
         )
     }
@@ -142,20 +202,52 @@ const Login = ({ navigation }) => {
     return (
         <>
             <View style={styles.mainContainer}>
-                <View style={{ opacity: modalVisible ? 0.1 : 1, }}>
-                    <View style={{ marginLeft: width * 0.21 }}>
-                        <Image
-                            style={{ width: 164, height: 72, resizeMode: 'contain' }}
-                            source={require('../Assets/Rentals1.png')}
-                        />
-                    </View>
-                    <Text style={styles.loginTxt}>
-                        Login
-                    </Text>
-                    <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
-                        <Input iconName="phone" placeholder="Mobile Number" keyboardType="numeric" error={error.mobileEmail} onFocus={() => handleErrors(null, "mobileEmail")} onChangeText={(txt) => handleValues(txt, 'mobileEmail')} />
+                <View style={{ opacity: modalVisible ? 0 : 1, }}>
+                    <View style={styles.upperContainer}>
+                        <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+                            <Image
+                                style={{ width: 164, height: 72, resizeMode: 'contain' }}
+                                source={require('../Assets/Rentals1.png')}
+                            />
+                        </View>
+                        <View>
+                            <Text style={styles.loginTxt}>
+                                Welcome,
+                            </Text>
+                            <Text style={styles.loginSubTxt}>
+                                Generate Otp
+                            </Text>
+                        </View>
+                        <View style={styles.subContainer}>
+                            <View>
+                                <Input iconName="phone" placeholder="Mobile Number" keyboardType="numeric" error={error.mobileEmail} onFocus={() => handleErrors(null, "mobileEmail")} onChangeText={(txt) => handleValues(txt, 'mobileEmail')} />
 
-                        <AppButton onPress={handleOtpClick} btnWidth={0.78} buttonText={'Get Otp'} bgColor='#2980b9' borderRadius={24} />
+                                <AppButton onPress={handleOtpClick} btnWidth={0.78} buttonText={'Get Otp'} bgColor='#2980b9' borderRadius={24} />
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{ marginTop: 12, }}>
+                        <Text style={{ color: '#000', fontSize: 24, textAlign: 'center', }}>
+                            OR
+                        </Text>
+                    </View>
+                    <View style={styles.lowerContainer}>
+                        <View style={styles.lowerSubContainer}>
+                            <Icon name='facebook' style={{ fontSize: 32, color: '#000', marginLeft: 8, }} />
+                            <View style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+                                <Text style={{ color: '#000', fontSize: 22, }}>
+                                    Continue with Facebook
+                                </Text>
+                            </View>
+                        </View>
+                        <View style={styles.lowerSubContainer}>
+                            <Icon name='google' style={{ fontSize: 30, color: '#000', marginLeft: 8, }} />
+                            <View style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+                                <Text style={{ color: '#000', fontSize: 22, }}>
+                                    Continue with Google
+                                </Text>
+                            </View>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -166,28 +258,68 @@ const Login = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     mainContainer: {
+        height: height * 1,
+        width: width * 1,
+        borderRadius: 16,
+        marginTop: height * 0.01,
+        backgroundColor: '#f2f2f2',
+        display: 'flex',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+
+    subContainer: {
         display: 'flex',
         justifyContent: 'center',
-        alignContent: 'center',
-        marginTop: 24,
-        height: height * 0.32,
-        width: width * 0.84,
-        borderRadius: 16,
-        marginTop: '32%',
         marginLeft: 'auto',
         marginRight: 'auto',
-        backgroundColor: '#f2f2f2',
+    },
+
+    upperContainer: {
+        backgroundColor: "#fff",
+        padding: 8,
+        borderRadius: 16,
+        elevation: 2,
+        height: height * 0.64,
+        width: width * 0.92,
+    },
+
+    lowerContainer: {
+        height: height * 0.32,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: height * 0.016,
+    },
+
+    lowerSubContainer: {
+        width: width * 0.86,
+        height: height * 0.08,
+        borderColor: '#000',
+        borderWidth: 0.5,
+        marginTop: 12,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
     },
 
     loginTxt: {
         marginTop: 16,
         paddingVertical: 8,
-        color: '#20232a',
-        textAlign: 'center',
-        fontSize: 22,
-        fontWeight: 'bold',
+        color: '#000',
+        fontSize: 32,
+        fontWeight: 900,
         fontFamily: 'Poppins',
-        marginBottom: 4,
+        marginLeft: 4,
+    },
+
+    loginSubTxt: {
+        paddingVertical: 8,
+        color: '#353b48',
+        fontSize: 20,
+        fontWeight: 500,
+        fontFamily: 'Poppins',
+        marginLeft: 4,
+        marginTop: -8,
     },
 
     centeredView: {
@@ -199,10 +331,9 @@ const styles = StyleSheet.create({
     },
 
     modalView: {
-        backgroundColor: 'white',
+        backgroundColor: '#fff',
         borderRadius: 20,
         padding: 18,
-        alignItems: 'center',
         shadowColor: '#000',
         shadowOffset: {
             width: 0,
@@ -211,23 +342,35 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 4,
         elevation: 5,
-        height: height * 0.36,
-        width: width * 0.72,
+        height: height * 0.42,
+        width: width * 0.84,
+    },
+
+    innerContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: 12,
+        alignItems: 'center',
+    },
+
+    otpMainContainer: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
     },
 
     otpContainer: {
-        width: width * 0.48,
-        backgroundColor: '#fff',
-        borderRadius: 32,
-        borderWidth: 1,
-        borderColor: '#3498db',
+        width: width * 0.1,
+        borderWidth: 0.8,
+        borderColor: '#000',
         padding: 10,
-        marginTop: 12,
+        marginTop: 24,
         display: 'flex',
         flexDirection: 'row',
-        alignItems: 'center',
         marginBottom: 6,
         marginLeft: 12,
+        height: height * 0.07,
+        backgroundColor: '#fff',
     },
 });
 
